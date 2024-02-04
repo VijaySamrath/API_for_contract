32e64fa4d81cccde6d285c6e77c02570437a0d1d
vijay kiran samrath <vijaysamrath462@gmail.com>
2024-02-04 21:41:22 +0530

    UPdate code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const Web3 = require("web3");
-const MyContract = require("/MyContract.json");
+const{Web3} = require("web3");
+const MyContract = require("./MyContract.json");
 const contractABI = MyContract.abi;
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const rpcEndpoint = "http://127.0.0.1:8545";
 
 const app = express();
-const web3 = new Web3(new Web3.provoders.HttpProvider(rpcEndpoint));
+const web3 = new Web3(new Web3.providers.HttpProvider(rpcEndpoint));
 
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
@@ -21,7 +21,7 @@ app.post("/number", async (req, res) => {
     const { number } = req.body;
     const accounts = await web3.eth.getAccounts();
     const result = await contract.methods
-      .sendNumber(number)
+      .setNumber(number)
       .send({ from: accounts[0]});
     res.json({ message: "number set successfully"});
 })
